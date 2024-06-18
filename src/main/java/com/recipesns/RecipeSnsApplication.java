1397796bd373cb397d565cc8ab207b25a080dd8b
kimbro97 <rlagudwog@gmail.com>
2024-06-18 18:39:37 +0900

    feat: project 생성

---
+++ src/main/java/com/recipesns/RecipeSnsApplication.java
@@ -0,0 +1,13 @@
+package com.recipesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipesns/RecipeSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
