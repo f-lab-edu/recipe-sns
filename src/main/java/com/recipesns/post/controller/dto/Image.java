ea46f710b7d2163c0461a19028c0b56e2e243724
kimbro97 <rlagudwog@gmail.com>
2024-08-12 11:47:54 +0900

    refactor: setter 삭제

--- src/main/java/com/recipesns/post/controller/dto/Image.java
+++ src/main/java/com/recipesns/post/controller/dto/Image.java
@@ -1,10 +1,8 @@
 package com.recipesns.post.controller.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class Image {
     private String filePath;
     private String fileName;
